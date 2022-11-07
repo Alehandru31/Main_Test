66798521539e507a8f22bcd01af85f9ecb61ec78
Alehandru31 <Alehandru753202@mail.ru>
2022-11-07 23:52:03 +0200

    ITProger 3.2 Problems

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       int[] arr =new int[]{5, 7, 8, 10};
+        getElementsOfArray(arr);
+    }
+    public static void getElementsOfArray(int[] numer){
+        System.out.println(numer[0]+"/"+numer[numer.length-1]);
+    }
+}
