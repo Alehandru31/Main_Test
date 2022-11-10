ce88e56cee083a3ae5a21904cb4b615dce8cef3e
Alehandru31 <Alehandru753202@mail.ru>
2022-11-10 21:34:27 +0200

    Invatat crearea metodelor claselor

--- Lessons/ITProger Lesson13/src/Cars.java
+++ Lessons/ITProger Lesson13/src/Cars.java
@@ -1,26 +1,33 @@
-public class Cars {
-    public float speed;
-    public int weight;
-    public String colors;
-    public byte[] coordinate;
+ public class Cars {
+   private float speed;
+   private int weight;
+    private String colors;
+    private byte[] coordinate;
+// Mai jos noi vedem constructor pentru Cars care primeste parametrii de la Honda
+     public Cars(float speed, int weight, String colors, byte[] coordinate) { //se formeaza un constructor cu modificator dostupa public si primeste 4 parametri
+         System.out.println("Honda: ");
+         setCars(speed, weight, colors, coordinate); //apelam la setCar Unde instalam prin parametri speed, in palea clasa (this.speed) datele de la metod
+         System.out.println(getCars()); //Citim setarile facute in getCars
+     }
+     public Cars(float speed, byte[] coordinate){
+         System.out.println("Bmw: ");
+         this.speed=speed;
+         this.coordinate=coordinate;
+         System.out.println(getCars());
+     }
 
-    public Cars(float speed, int weight, String colors, byte[] coordinate){
-        System.out.println("Honda -");
-        setCars(this.speed, this.weight,this.colors, this.coordinate);
-        System.out.print(getCars());
-    }
-    public void setCars(float speed, int weight, String colors, byte[] coordinate){
+     public void setCars(float speed, int weight, String colors, byte[] coordinate){
         this.colors=colors;
         this.weight=weight;
         this.speed=speed;
         this.coordinate=coordinate;
     }
+
     public String getCars(){
-        String infoCoordinates = "Coordinats: ";
-        for(int i = 0; i < coordinate.length; i++) {
-            infoCoordinates += coordinate[i] + ",";
-        }
+         String infocoordinate="Coordonats: \n";
+         for (int i=0; i<coordinate.length; i++)
+             infocoordinate +=coordinate[i]+"\n";
         String info ="Car iz speed-:" +this.speed + ";\n " + "Massa iz- "+ this.weight +".\n" + "Colorss iz -" +this.colors +".\n";
-        return info +infoCoordinates;
+        return info+infocoordinate;
     }
 }
--- Lessons/ITProger Lesson13/src/Lesson13.java
+++ Lessons/ITProger Lesson13/src/Lesson13.java
@@ -2,15 +2,9 @@
 
 public class Lesson13 {
     public static void main(String[] args) {
-        Cars honda=new Cars(250.05f, 2100, "Red", new byte[]{100,0,0});
 
-//        Cars bmw=new Cars();
-//         bmw.speed=300.5f;
-//         bmw.weight=1800;
-//         bmw.colors="Black";
-//         bmw.coordinate=new byte[]{0,10,120};
-//
-//        System.out.println(bmw.getCars());
-//        System.out.println(honda.getCars());
+        Cars honda = new Cars(250.5f, 2500, "White", new byte[] {0, 0, 0});
+
+        Cars bmw = new Cars(280.05f,new byte[] {100, 25,45});
     }
 }
