bc66f307603e851dc8f2b667099493922279958e
Alehandru31 <Alehandru753202@mail.ru>
2022-11-10 23:30:47 +0200

    Lectia14

--- Lessons/ITProger Lesson13/src/Cars.java
+++ Lessons/ITProger Lesson13/src/Cars.java
@@ -4,9 +4,11 @@
     private String colors;
     private byte[] coordinate;
 // Mai jos noi vedem constructor pentru Cars care primeste parametrii de la Honda
-     public Cars(float speed, int weight, String colors, byte[] coordinate) { //se formeaza un constructor cu modificator dostupa public si primeste 4 parametri
+     public Cars(float speed, int weight, String colors, byte[] coordinate) { //se formeaza un constructor cu
+         // modificator dostupa public si primeste 4 parametri
          System.out.println("Honda: ");
-         setCars(speed, weight, colors, coordinate); //apelam la setCar Unde instalam prin parametri speed, in palea clasa (this.speed) datele de la metod
+         setCars(speed, weight, colors, coordinate); //apelam la setCar Unde instalam prin
+         // parametri speed, in palea clasa (this.speed) datele de la metod
          System.out.println(getCars()); //Citim setarile facute in getCars
      }
      public Cars(float speed, byte[] coordinate){
@@ -27,7 +29,8 @@
          String infocoordinate="Coordonats: \n";
          for (int i=0; i<coordinate.length; i++)
              infocoordinate +=coordinate[i]+"\n";
-        String info ="Car iz speed-:" +this.speed + ";\n " + "Massa iz- "+ this.weight +".\n" + "Colorss iz -" +this.colors +".\n";
+        String info ="Car iz speed-:" +this.speed + ";\n " + "Massa iz- "+ this.weight +".\n" +
+                "Colorss iz -" +this.colors +".\n";
         return info+infocoordinate;
     }
 }
