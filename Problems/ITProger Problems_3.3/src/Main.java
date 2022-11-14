370ba92935fcd6c1cd0d7e66bf7cd0eb5a0d1e6c
Alehandru31 <Alehandru753202@mail.ru>
2022-11-14 12:04:13 +0200

    Rezolvata

--- Problems/ITProger Problems_3.3/src/Main.java
+++ Problems/ITProger Problems_3.3/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        divideAndRound( 500.05d,2.25d,3.01d);
+    }
+
+    public static double divideAndRound(double a, double b, double c) {
+        double divide=Math.ceil(a/b/c);
+        System.out.println("Результат: " + divide);
+        return divide ;
     }
 }
