c65270f497d477d54713ee542afbdf31990abf03
Alehandru31 <Alehandru753202@mail.ru>
2022-11-14 15:40:30 +0200

    rezolvare

--- Problems/ITProger Problems_3.4/src/Main.java
+++ Problems/ITProger Problems_3.4/src/Main.java
@@ -1,6 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
 public static void main(String[] args){
-
-  }
+  ArrayList<Integer> numer=new ArrayList<>();
+  numer.add(5);
+  numer.add(6);
+  numer.add(20);
+  numer.add(1);
+ for(int i: numer)
+   System.out.println("El: "+i);
+}
 }
 
