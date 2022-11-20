c20ce1444d94d16b0f29b85dbfe0a1482d7f6069
Alehandru31 <Alehandru753202@mail.ru>
2022-11-20 15:48:40 +0200

    Rezolvat

--- Problems/ITProger Problems_3.9/src/HashMaps.java
+++ Problems/ITProger Problems_3.9/src/HashMaps.java
@@ -1,15 +1,29 @@
+//Создайте список данных на основе класса «HashMap». Добавьте в него ключи и значения, что представлены ниже:
+//
+//"name": "Alex",
+//"age": "45",
+//"hobby": "football"
+//Выведите все элементы на экран в формате: «Ключ: key. Значение: value», где key – ключ элемента, а value – значение элемента.
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class HashMaps {
-    public static void main(String[] args) {
-        HashMap<String, String> data = new HashMap<String, String>();
-        data.put("name", "Alex");
-        data.put("age", "45");
-        data.put("hobby", "football");
+public static void main (String[] args) {
+    // Создаем список данных
+    Map<String, String> numbers = new HashMap<>();
 
-        for(String i: data.keySet()) {
-            System.out.println("Ключ: " + i + " Значение: " + data.get(i));
-        }
+    // Помещаем данные на карточку
+    numbers.put("name", "Alex");
+    numbers.put("age", "45");
+    numbers.put("hobby", "football");
+
+    // Для получения ключей и значений
+    // нужно использовать Map.Entry и
+    // метод entrySet()
+    for (Map.Entry i : numbers.entrySet()) {
+        System.out.println("Ключ: " + i.getKey() + ". Значение: "
+                + i.getValue());
     }
+}
 }
