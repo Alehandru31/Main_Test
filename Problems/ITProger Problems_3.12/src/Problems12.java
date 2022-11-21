8a2d1a7d940797033bae453d8704fc39a20a6abb
Alehandru31 <Alehandru753202@mail.ru>
2022-11-21 13:23:57 +0200

    Rezolvat

---
+++ Problems/ITProger Problems_3.12/src/Problems12.java
@@ -0,0 +1,10 @@
+//Допишите в класс «Study» конструктор, что будет принимать один параметр и будет устанавливать значение этого параметра в поле «course».
+//
+//Создайте объект класса «Study». В качестве значения для поля «course» установите текст: «Изучение Java - это просто!». Обратитесь к методу «printCourse» для вывода значения поля «course».
+
+public class Problems12 {
+    public static void main(String[] args) {
+     Study study=new Study("Изучение Java - это просто!");
+        System.out.println(study.printCourse());
+    }
+}
