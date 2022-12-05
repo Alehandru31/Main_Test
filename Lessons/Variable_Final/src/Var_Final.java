df3838b8578d85474871ddf4085d18086ed7c9de
Alehandru31 <Alehandru753202@mail.ru>
2022-12-05 23:51:58 +0200

    Corect

---
+++ Lessons/Variable_Final/src/Var_Final.java
@@ -0,0 +1,10 @@
+public class Var_Final {
+    public static void main(String[] args) {
+        final int I=10;
+        System.out.println(I);
+    }
+}
+
+class Test{
+     private static final int CONSTANT=10;
+}
