a9f08ce0141bab10ec331832a7c36f7f709938f0
Alex <Alehandru753202@mail.ru>
2022-12-15 13:59:29 +0200

    Corect

--- Difereite exemple/Citirea unui File/src/Main.java
+++ Difereite exemple/Citirea unui File/src/Main.java
@@ -3,6 +3,7 @@
 // (powered by FernFlower decompiler)
 //
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -14,7 +15,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
        // Prima metoda de indicare a adresei pina la file
         String select = File.separator;
-        String puty = select + "D:" + select + "Java" + select + "Proecte java" + select + "Main_Test" + select + "Difereite exemple" + select + "Citirea unui File" + select + "Test.txt";
+       String puty = select + "D:" + select + "Java" + select + "Proecte java" + select + "Main_Test" + select + "Difereite exemple" + select + "Citirea unui File" + select + "Test.txt";
         File file = new File(puty);
 
         Scanner scanner = new Scanner(file);
